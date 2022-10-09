f1ff74908569d7268fcdc1d4f28adcd707ca710c
Tim Alber <t.midaidu@gmail.com>
2022-10-09 21:58:40 +0200

    added new page

--- lib/home/open-challenges.dart
+++ lib/home/open-challenges.dart
@@ -10,6 +10,24 @@ class OpenChallenges extends StatefulWidget {
 class _OpenChallengesState extends State<OpenChallenges> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Offene Herausforderungen');
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home)),
+              Tab(icon: Icon(Icons.favorite)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            //MyHomePage(),
+            //FavoritePage(),
+          ],
+        ),
+      ),
+    );
   }
 }
